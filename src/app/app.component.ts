2ab2232ba78226e6461a6ce7a21a692fa275ab1f
sandraelsa16 <sandraelsavarughese@gmail.com>
2025-06-11 17:13:06 +0530

    changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { SidebarComponent } from "./sidebar/sidebar.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ LoginComponent],
+  imports: [ RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,35 +1,65 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { KENDO_BUTTONS } from '@progress/kendo-angular-buttons';
+import { KENDO_TEXTBOX } from '@progress/kendo-angular-inputs';
+import { KENDO_LABEL } from '@progress/kendo-angular-label';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule,CommonModule,KENDO_LABEL,KENDO_BUTTONS,KENDO_TEXTBOX],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-   loginForm:FormGroup
+  //  loginForm:FormGroup
  
-  constructor(private router:Router, private fb:FormBuilder){
-    this.loginForm = this.fb.group({
-      username:['',[Validators.required]],
-      password:['',[Validators.required,Validators.pattern('[a-zA_Z0-9]*')]]
-    })
+  // constructor(private router:Router, private fb:FormBuilder){
+  //   this.loginForm = this.fb.group({
+  //     username:['',[Validators.required]],
+  //     password:['',[Validators.required,Validators.pattern('^[A-Za-z0-9]*$')]]
+  //   })
+  // }
+
+  // login(){
+  //   console.log("Login clicked");
+  //   if(this.loginForm.valid){
+  //     const username = this.loginForm.value.username
+  //     const password = this.loginForm.value.password
+  //     this.loginForm.reset()
+  //     sessionStorage.setItem('name',username)
+  //     this.router.navigateByUrl('/dashboard')
+     
+      
+  //   }else {
+      
+  //   }
+  // }
+
+
+  public form: FormGroup;
+
+  public data = {
+    username: "",
+    password: "",
+   
+  };
+  
+
+  constructor(private router: Router) {
+    this.form = new FormGroup({
+      username: new FormControl(this.data.username, [Validators.required]),
+      password: new FormControl(this.data.password, [
+        Validators.required,
+        Validators.pattern('^[A-Za-z0-9]*$'),
+      ]),
+    });
   }
 
-  login(){
-   
-    if(this.loginForm.valid){
-      const username = this.loginForm.value.username
-      const password = this.loginForm.value.password
-      this.loginForm.reset()
-      sessionStorage.setItem('name',username)
-      this.router.navigateByUrl('/dashboard')
-    }else {
-      
-    }
+  public submitForm(): void {
+    console.log(this.form.value);
+    this.router.navigate(["/dashboard"])
   }
 }
