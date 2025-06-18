dd412d555a2e77a5b0e864d353818e430d33f8d9
sandraelsa16 <sandraelsavarughese@gmail.com>
2025-06-18 10:33:28 +0530

    changes added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { TabsComponent } from "../tabs/tabs.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent,TabsComponent],
+  imports: [HeaderComponent, SidebarComponent,TabsComponent,CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  scheduler:Date = new Date();
+
+ formatDate: string = 
+    this.scheduler.getDate() + ' ' +
+    this.scheduler.toLocaleString('default', { month: 'short' }) + ' ' +
+    this.scheduler.getFullYear();
 
 }
